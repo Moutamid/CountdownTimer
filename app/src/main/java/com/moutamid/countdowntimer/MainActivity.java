6afb4628149ac8208fe24aa04eca4f2871acd6cf
suleman <sunnybajwa904@gmail.com>
2022-10-11 16:35:38 +0500

    Initial commit

---
+++ app/src/main/java/com/moutamid/countdowntimer/MainActivity.java
@@ -0,0 +1,76 @@
+package com.moutamid.countdowntimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    NumberPicker hourPicker, minPicker, secPicker;
+    int hour, min, sec;
+    Button start;
+    TextView oneH, tenM, tenS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hourPicker = findViewById(R.id.numberHour);
+        minPicker = findViewById(R.id.numberMin);
+        secPicker = findViewById(R.id.numberSec);
+        start = findViewById(R.id.startTimer);
+        oneH = findViewById(R.id.oneHour);
+        tenM = findViewById(R.id.tenMin);
+        tenS = findViewById(R.id.tenSec);
+
+        hourPicker.setMinValue(00);
+        hourPicker.setMaxValue(99);
+
+        minPicker.setMinValue(00);
+        minPicker.setMaxValue(59);
+
+        secPicker.setMinValue(00);
+        secPicker.setMaxValue(59);
+
+        hourPicker.setOnValueChangedListener((numberPicker, i, i1) -> hour = numberPicker.getValue());
+
+        minPicker.setOnValueChangedListener((numberPicker, i, i1) -> min = numberPicker.getValue());
+
+        secPicker.setOnValueChangedListener((numberPicker, i, i1) -> sec = numberPicker.getValue());
+
+        start.setOnClickListener(v -> {
+            hour = hourPicker.getValue();
+            min = minPicker.getValue();
+            sec = secPicker.getValue();
+            Intent i = new Intent(MainActivity.this, TimerActivity.class);
+            i.putExtra("hour", String.valueOf(hour));
+            i.putExtra("min", String.valueOf(min));
+            i.putExtra("sec", String.valueOf(sec));
+            startActivity(i);
+        });
+
+        oneH.setOnClickListener(v -> {
+            hourPicker.setValue(1);
+            minPicker.setValue(0);
+            secPicker.setValue(0);
+        });
+
+        tenM.setOnClickListener(v -> {
+            hourPicker.setValue(0);
+            minPicker.setValue(10);
+            secPicker.setValue(0);
+        });
+
+        tenS.setOnClickListener(v -> {
+            hourPicker.setValue(0);
+            minPicker.setValue(0);
+            secPicker.setValue(10);
+        });
+
+    }
+}
---
+++ app/src/main/java/com/moutamid/countdowntimer/TimerActivity.java
@@ -0,0 +1,92 @@
+package com.moutamid.countdowntimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class TimerActivity extends AppCompatActivity {
+    String hour, min, sec;
+    TextView hourTV, minTV, secTV;
+    int duration;
+    Button cancel, pause;
+    boolean state = false;
+    Ringtone ringtone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        ringtone = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+
+        hour = getIntent().getStringExtra("hour");
+        min = getIntent().getStringExtra("min");
+        sec = getIntent().getStringExtra("sec");
+
+        hourTV = findViewById(R.id.hourTV);
+        minTV = findViewById(R.id.minTV);
+        secTV = findViewById(R.id.secTV);
+        cancel = findViewById(R.id.cancel);
+        pause = findViewById(R.id.pause);
+
+        duration = (Integer.parseInt(hour) * 60 * 60) + (Integer.parseInt(min) * 60) + Integer.parseInt(sec);
+
+        CountDownTimer timer = new CountDownTimer(duration * 1000, 1000){
+
+            @Override
+            public void onTick(long l) {
+
+                runOnUiThread(() -> {
+ 
+                    String time = String.format(Locale.getDefault(),"%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(l),
+                            TimeUnit.MILLISECONDS.toMinutes(l) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(l)),
+                            TimeUnit.MILLISECONDS.toSeconds(l) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(l)));
+                    final String[] hourMinSec = time.split(":");
+                    hourTV.setText(hourMinSec[0]);
+                    minTV.setText(hourMinSec[1]);
+                    secTV.setText(hourMinSec[2]);
+                });
+            }
+
+            @Override
+            public void onFinish() {
+                ringtone.play();
+            }
+        };
+
+        timer.start();
+
+        pause.setOnClickListener(v -> {
+            if (state){
+                timer.cancel();
+                pause.setText("Resume");
+                state = false;
+            } else {
+                int hour = Integer.parseInt(hourTV.getText().toString());
+                int min = Integer.parseInt(minTV.getText().toString());
+                int sec = Integer.parseInt(secTV.getText().toString());
+                Toast.makeText(this, ""+sec, Toast.LENGTH_SHORT).show();
+                duration = (hour * 60 * 60) + (min * 60) + sec;
+                timer.start();
+                pause.setText("Pause");
+                state = true;
+            }
+        });
+
+        cancel.setOnClickListener(v -> {
+            startActivity(new Intent(TimerActivity.this, MainActivity.class));
+            finish();
+            ringtone.stop();
+        });
+    }
+}
